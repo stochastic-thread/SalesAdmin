1b45d52b3faaa7e98f1197384f9f760bba8fffb2
stochastic-thread <hello@arthurcolle.com>
2019-04-09 16:46:09 -0400

    Changed front end upload

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :pages
   resources :fileuploads
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "/", to: 'pages#_'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,54 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_08_193755) do
+ActiveRecord::Schema.define(version: 2019_04_09_002755) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "active_admin_comments", force: :cascade do |t|
-    t.string "namespace"
-    t.text "body"
-    t.string "resource_type"
-    t.bigint "resource_id"
-    t.string "author_type"
-    t.bigint "author_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
-    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
-    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
-  end
-
-  create_table "admin_users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_admin_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
-  end
-
-  create_table "categories", force: :cascade do |t|
-    t.string "label"
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "cryptocurrency_payloads", force: :cascade do |t|
-    t.string "denomination"
-    t.bigint "user_id"
-    t.string "address"
-    t.string "privatekey"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.boolean "active"
-    t.index ["user_id"], name: "index_cryptocurrency_payloads_on_user_id"
-  end
 
   create_table "fileuploads", force: :cascade do |t|
     t.string "fname"
@@ -68,32 +24,8 @@ ActiveRecord::Schema.define(version: 2019_04_08_193755) do
   end
 
   create_table "pages", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "products", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.integer "price"
-    t.string "currency"
-    t.bigint "shop_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["shop_id"], name: "index_products_on_shop_id"
-  end
-
-  create_table "reviews", force: :cascade do |t|
-    t.bigint "product_id"
-    t.bigint "user_id"
-    t.integer "rating"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["product_id"], name: "index_reviews_on_product_id"
-    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "sales", force: :cascade do |t|
@@ -107,45 +39,4 @@ ActiveRecord::Schema.define(version: 2019_04_08_193755) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "shops", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.string "country"
-    t.string "ships_to"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_shops_on_user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "handle"
-    t.integer "pin"
-    t.text "pgp_privatekey"
-    t.text "pgp_publickey"
-    t.boolean "can_view"
-    t.boolean "can_buy"
-    t.boolean "can_list"
-    t.string "btc_address"
-    t.string "btc_privatekey"
-    t.string "zec_address"
-    t.string "zec_privatekey"
-    t.string "eth_address"
-    t.string "eth_privatekey"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-  add_foreign_key "cryptocurrency_payloads", "users"
-  add_foreign_key "products", "shops"
-  add_foreign_key "reviews", "products"
-  add_foreign_key "reviews", "users"
-  add_foreign_key "shops", "users"
 end
